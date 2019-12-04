4318da84d172a222fbca86b6423910b79320b430
afifahnaimatul <naimatul.afifah@gmail.com>
2019-12-04 12:30:56 +0700

    Connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oke/wrapper.dart';
 import 'package:oke/presenter/Presenter.dart';
 import 'package:oke/view/Login.dart';
 import 'package:oke/view/View.dart';
@@ -13,8 +14,22 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new LoginPage(),
+      home: Wrapper(),
       routes: {'/login' : (_) => new Pages()},
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: new ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: new LoginPage(),
+//       routes: {'/login' : (_) => new Pages()},
+//     );
+//   }
+// }
---
+++ lib/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:oke/view/Login.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return both home or auth widget
+    return Pages();
+  }
+}
